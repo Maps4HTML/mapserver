df03fe9952cb429c9287d72fdc86c1e45a2474da
Thomas Bonfort <thomas.bonfort@gmail.com>
2013-03-18 18:16:19 +0100

    more standard xmp detection

--- cmake/FindExempi.cmake
+++ cmake/FindExempi.cmake
@@ -1,22 +1,22 @@
-# LIBEXEMPI_FOUND - system has the LIBEXEMPI library
-# LIBEXEMPI_INCLUDE_DIR - the LIBEXEMPI include directory
-# LIBEXEMPI_LIBRARIES - The libraries needed to use LIBEXEMPI
-
-#in cache already
-if(LIBEXEMPI_INCLUDE_DIR AND LIBEXEMPI_LIBRARIES)
-  set(LIBEXEMPI_FIND_QUIETLY TRUE)
-endif(LIBEXEMPI_INCLUDE_DIR AND LIBEXEMPI_LIBRARIES)
-
-include(FindPkgConfig)
-pkg_check_modules(LIBEXEMPI exempi-2.0)
-
-find_path(LIBEXEMPI_INCLUDE_DIR
-    NAMES xmp.h
+# Look for the header file.
+find_path(LIBEXEMPI_INCLUDE_DIR NAMES xmp.h
     PATH_SUFFIXES exempi-2.0 exempi-2.0/exempi exempi 
-    PATHS ${LIBEXEMPI_INCLUDEDIR})
-find_library(LIBEXEMPI_LIBRARIES NAMES exempi ${LIBEXEMPI_LIBRARY_DIRS})
+)
 
+# Look for the library.
+find_library(LIBEXEMPI_LIBRARY NAMES exempi)
+
+# Handle the QUIETLY and REQUIRED arguments and set FCGI_FOUND to TRUE if all listed variables are TRUE.
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(libexempi DEFAULT_MSG LIBEXEMPI_INCLUDE_DIR LIBEXEMPI_LIBRARIES)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(LIBEXEMPI DEFAULT_MSG LIBEXEMPI_LIBRARY LIBEXEMPI_INCLUDE_DIR)
 
-mark_as_advanced(LIBEXEMPI_INCLUDE_DIR LIBEXEMPI_LIBRARIES)
+# Copy the results to the output variables.
+if(LIBEXEMPI_FOUND)
+	set(LIBEXEMPI_LIBRARIES ${LIBEXEMPI_LIBRARY})
+	set(LIBEXEMPI_INCLUDE_DIRS ${LIBEXEMPI_INCLUDE_DIR})
+else()
+	set(LIBEXEMPI_LIBRARIES)
+	set(LIBEXEMPI_INCLUDE_DIRS)
+endif()
+
+mark_as_advanced(LIBEXEMPI_INCLUDE_DIR LIBEXEMPI_LIBRARY)
